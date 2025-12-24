3c36f5aa163dfe89ad7522d1fdece9d94673d63f
Sabari <sabari.biju.a51@gmail.com>
2025-12-24 21:57:16 +0530

    Tried out Ridge Regression

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+import mlflow
+import mlflow.sklearn
+from sklearn.linear_model import Ridge
 from sklearn.metrics import root_mean_squared_error
 import joblib
 
@@ -10,18 +12,32 @@ print(f"Number of rows = {df.shape[0]}")
 print(f"Number of columns = {df.shape[1]}")
 
 def train_model():
-    X_train, X_val, y_train, y_val, preprocessor = prepare_data("data/raw/train.csv")
+    X_train, X_val, y_train, y_val, preprocessor = prepare_data(
+        "data/raw/train.csv"
+    )
 
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+    with mlflow.start_run():
 
-    preds = model.predict(X_val)
-    rmse = root_mean_squared_error(y_val, preds)
+        model = Ridge(alpha=0.80, random_state=43)
+        model.fit(X_train, y_train)
 
-    print(f"Validation RMSE: {rmse:.2f}")
+        preds = model.predict(X_val)
+        rmse = root_mean_squared_error(y_val, preds)
 
-    joblib.dump(model, "model.joblib")
-    joblib.dump(preprocessor, "preprocessor.joblib")
+
+        mlflow.log_param("model_type", "Ridge")
+        mlflow.log_param("alpha", 0.80)
+        mlflow.log_metric("rmse", rmse)
+
+        joblib.dump(model, "model.joblib")
+        joblib.dump(preprocessor, "preprocessor.joblib")
+
+        mlflow.log_artifact("model.joblib")
+        mlflow.log_artifact("preprocessor.joblib")
+        mlflow.log_param("target_transform", "log1p")
+
+
+        print(f"Validation RMSE (Ridge): {rmse:.2f}")
 
 
 if __name__ == "__main__":
